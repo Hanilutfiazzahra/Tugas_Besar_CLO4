533a9fc7a691feceadc4c190384e9a84576cf077
Hanilutfiazzahra <hanilutfiazzahraa@gmail.com>
2026-06-18 22:07:32 +0700

    menambahkann GUI Jadwal Lapangan dan LapanganService

--- Tugas_Besar_CLO4/Forms/Customer/FormJadwalLapangan.Designer.cs
+++ Tugas_Besar_CLO4/Forms/Customer/FormJadwalLapangan.Designer.cs
@@ -30,16 +30,19 @@
         {
             lblTitle = new Label();
             lblTipe = new Label();
-            btnRegular = new Button();
-            btnVIP = new Button();
             lblPilihan = new Label();
             dgnJadwal = new DataGridView();
+            clmJadwal1 = new DataGridViewTextBoxColumn();
+            clmStatus2 = new DataGridViewTextBoxColumn();
+            clmJadwal2 = new DataGridViewTextBoxColumn();
+            clmStatus1 = new DataGridViewTextBoxColumn();
             btnKembali = new Button();
             lblLanjutReservasi = new Button();
             lblHarga = new Label();
             lblFasilitas = new Label();
-            colJam = new DataGridViewTextBoxColumn();
-            colStatus = new DataGridViewTextBoxColumn();
+            lblTanggal = new Label();
+            lblGedung = new Label();
+            cmbTipeLapangan = new ComboBox();
             ((System.ComponentModel.ISupportInitialize)dgnJadwal).BeginInit();
             SuspendLayout();
             // 
@@ -47,7 +50,7 @@
             // 
             lblTitle.AutoSize = true;
             lblTitle.Font = new Font("Segoe UI", 18F);
-            lblTitle.Location = new Point(388, 29);
+            lblTitle.Location = new Point(516, 28);
             lblTitle.Name = "lblTitle";
             lblTitle.Size = new Size(287, 41);
             lblTitle.TabIndex = 0;
@@ -59,35 +62,17 @@
             // 
             lblTipe.AutoSize = true;
             lblTipe.Font = new Font("Segoe UI", 10F);
-            lblTipe.Location = new Point(107, 128);
+            lblTipe.Location = new Point(126, 222);
             lblTipe.Name = "lblTipe";
             lblTipe.Size = new Size(163, 23);
             lblTipe.TabIndex = 1;
             lblTipe.Text = "Pilih Tipe Lapangan:";
             lblTipe.Click += label2_Click;
             // 
-            // btnRegular
-            // 
-            btnRegular.Location = new Point(107, 172);
-            btnRegular.Name = "btnRegular";
-            btnRegular.Size = new Size(94, 29);
-            btnRegular.TabIndex = 2;
-            btnRegular.Text = "Regular";
-            btnRegular.UseVisualStyleBackColor = true;
-            // 
-            // btnVIP
-            // 
-            btnVIP.Location = new Point(107, 219);
-            btnVIP.Name = "btnVIP";
-            btnVIP.Size = new Size(94, 29);
-            btnVIP.TabIndex = 3;
-            btnVIP.Text = "VIP";
-            btnVIP.UseVisualStyleBackColor = true;
-            // 
             // lblPilihan
             // 
             lblPilihan.AutoSize = true;
-            lblPilihan.Location = new Point(107, 277);
+            lblPilihan.Location = new Point(126, 333);
             lblPilihan.Name = "lblPilihan";
             lblPilihan.Size = new Size(208, 20);
             lblPilihan.TabIndex = 4;
@@ -101,27 +86,60 @@
             dgnJadwal.AllowUserToResizeColumns = false;
             dgnJadwal.AllowUserToResizeRows = false;
             dgnJadwal.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgnJadwal.Columns.AddRange(new DataGridViewColumn[] { colJam, colStatus });
-            dgnJadwal.Location = new Point(518, 119);
+            dgnJadwal.Columns.AddRange(new DataGridViewColumn[] { clmJadwal1, clmStatus2, clmJadwal2, clmStatus1 });
+            dgnJadwal.Location = new Point(516, 126);
             dgnJadwal.Name = "dgnJadwal";
             dgnJadwal.ReadOnly = true;
             dgnJadwal.RowHeadersWidth = 51;
             dgnJadwal.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgnJadwal.Size = new Size(412, 333);
+            dgnJadwal.Size = new Size(740, 240);
             dgnJadwal.TabIndex = 5;
+            // 
+            // clmJadwal1
+            // 
+            clmJadwal1.HeaderText = "Jadwal 1";
+            clmJadwal1.MinimumWidth = 6;
+            clmJadwal1.Name = "clmJadwal1";
+            clmJadwal1.ReadOnly = true;
+            clmJadwal1.Width = 170;
+            // 
+            // clmStatus2
+            // 
+            clmStatus2.HeaderText = "Status";
+            clmStatus2.MinimumWidth = 6;
+            clmStatus2.Name = "clmStatus2";
+            clmStatus2.ReadOnly = true;
+            clmStatus2.Width = 170;
+            // 
+            // clmJadwal2
+            // 
+            clmJadwal2.HeaderText = "Jadwal 2";
+            clmJadwal2.MinimumWidth = 6;
+            clmJadwal2.Name = "clmJadwal2";
+            clmJadwal2.ReadOnly = true;
+            clmJadwal2.Width = 170;
+            // 
+            // clmStatus1
+            // 
+            clmStatus1.HeaderText = "Status";
+            clmStatus1.MinimumWidth = 6;
+            clmStatus1.Name = "clmStatus1";
+            clmStatus1.ReadOnly = true;
+            clmStatus1.Width = 170;
             // 
             // btnKembali
             // 
-            btnKembali.Location = new Point(107, 406);
+            btnKembali.Location = new Point(967, 400);
             btnKembali.Name = "btnKembali";
             btnKembali.Size = new Size(94, 29);
             btnKembali.TabIndex = 6;
             btnKembali.Text = "Kembali";
             btnKembali.UseVisualStyleBackColor = true;
+            btnKembali.Click += btnKembali_Click;
             // 
             // lblLanjutReservasi
             // 
-            lblLanjutReservasi.Location = new Point(228, 406);
+            lblLanjutReservasi.Location = new Point(1112, 400);
             lblLanjutReservasi.Name = "lblLanjutReservasi";
             lblLanjutReservasi.Size = new Size(144, 29);
             lblLanjutReservasi.TabIndex = 7;
@@ -132,7 +150,7 @@
             // lblHarga
             // 
             lblHarga.AutoSize = true;
-            lblHarga.Location = new Point(107, 319);
+            lblHarga.Location = new Point(126, 367);
             lblHarga.Name = "lblHarga";
             lblHarga.Size = new Size(67, 20);
             lblHarga.TabIndex = 8;
@@ -142,42 +160,56 @@
             // lblFasilitas
             // 
             lblFasilitas.AutoSize = true;
-            lblFasilitas.Location = new Point(107, 357);
+            lblFasilitas.Location = new Point(126, 400);
             lblFasilitas.Name = "lblFasilitas";
             lblFasilitas.Size = new Size(77, 20);
             lblFasilitas.TabIndex = 9;
             lblFasilitas.Text = "Fasilitas : -";
             lblFasilitas.Click += lblFasilitas_Click;
             // 
-            // colJam
+            // lblTanggal
             // 
-            colJam.HeaderText = "Jam";
-            colJam.MinimumWidth = 6;
-            colJam.Name = "colJam";
-            colJam.ReadOnly = true;
-            colJam.Width = 180;
+            lblTanggal.AutoSize = true;
+            lblTanggal.Location = new Point(126, 126);
+            lblTanggal.Name = "lblTanggal";
+            lblTanggal.Size = new Size(72, 20);
+            lblTanggal.TabIndex = 10;
+            lblTanggal.Text = "Tanggal : ";
             // 
-            // colStatus
+            // lblGedung
             // 
-            colStatus.HeaderText = "Status";
-            colStatus.MinimumWidth = 6;
-            colStatus.Name = "colStatus";
-            colStatus.ReadOnly = true;
-            colStatus.Width = 180;
+            lblGedung.AutoSize = true;
+            lblGedung.Location = new Point(126, 160);
+            lblGedung.Name = "lblGedung";
+            lblGedung.Size = new Size(72, 20);
+            lblGedung.TabIndex = 11;
+            lblGedung.Text = "Gedung : ";
+            // 
+            // cmbTipeLapangan
+            // 
+            cmbTipeLapangan.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbTipeLapangan.FormattingEnabled = true;
+            cmbTipeLapangan.Items.AddRange(new object[] { "Regular", "VIP" });
+            cmbTipeLapangan.Location = new Point(126, 259);
+            cmbTipeLapangan.Name = "cmbTipeLapangan";
+            cmbTipeLapangan.Size = new Size(117, 28);
+            cmbTipeLapangan.TabIndex = 12;
+            cmbTipeLapangan.SelectedIndexChanged += cmbTipeLapangan_SelectedIndexChanged;
             // 
             // FormJadwalLapangan
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1079, 715);
+            ClientSize = new Size(1349, 715);
+            Controls.Add(cmbTipeLapangan);
+            Controls.Add(lblGedung);
+            Controls.Add(lblTanggal);
             Controls.Add(lblFasilitas);
             Controls.Add(lblHarga);
             Controls.Add(lblLanjutReservasi);
             Controls.Add(btnKembali);
             Controls.Add(dgnJadwal);
             Controls.Add(lblPilihan);
-            Controls.Add(btnVIP);
-            Controls.Add(btnRegular);
             Controls.Add(lblTipe);
             Controls.Add(lblTitle);
             Name = "FormJadwalLapangan";
@@ -192,15 +224,18 @@
 
         private Label lblTitle;
         private Label lblTipe;
-        private Button btnRegular;
-        private Button btnVIP;
         private Label lblPilihan;
         private DataGridView dgnJadwal;
         private Button btnKembali;
         private Button lblLanjutReservasi;
         private Label lblHarga;
         private Label lblFasilitas;
-        private DataGridViewTextBoxColumn colJam;
-        private DataGridViewTextBoxColumn colStatus;
+        private Label lblTanggal;
+        private Label lblGedung;
+        private ComboBox cmbTipeLapangan;
+        private DataGridViewTextBoxColumn clmJadwal1;
+        private DataGridViewTextBoxColumn clmStatus2;
+        private DataGridViewTextBoxColumn clmJadwal2;
+        private DataGridViewTextBoxColumn clmStatus1;
     }
 }
---
+++ Tugas_Besar_CLO4/Services/LapanganService.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tugas_Besar_CLO4.Entities;
+
+namespace Tugas_Besar_CLO4.Services
+{
+    public class LapanganService
+    {
+        public List<Lapangan> GetLapanganByGedung(
+            string gedung
+        )
+        {
+            List<Lapangan> daftarLapangan =
+                new List<Lapangan>()
+                {
+                    new LapanganRegular(
+                        "L001",
+                        "Court A",
+                        "Gedung A",
+                        50000
+                    ),
+
+                    new LapanganVIP(
+                        "L002",
+                        "Court B",
+                        "Gedung A",
+                        80000,
+                        new string[]
+                        {
+                            "AC",
+                            "LED",
+                            "Sofa"
+                        }
+                    ),
+
+                    new LapanganRegular(
+                        "L003",
+                        "Court C",
+                        "Gedung B",
+                        50000
+                    ),
+
+                    new LapanganVIP(
+                        "L004",
+                        "Court D",
+                        "Gedung B",
+                        80000,
+                        new string[]
+                        {
+                            "AC",
+                            "LED",
+                            "Sofa"
+                        }
+                    ),
+
+                    new LapanganRegular(
+                        "L005",
+                        "Court E",
+                        "Gedung C",
+                        50000
+                    ),
+
+                    new LapanganVIP(
+                        "L006",
+                        "Court F",
+                        "Gedung C",
+                        80000,
+                        new string[]
+                        {
+                            "AC",
+                            "LED",
+                            "Sofa"
+                        }
+                    )
+                };
+
+            return daftarLapangan
+                .Where(x => x.lokasi == gedung)
+                .ToList();
+        }
+    }
+}
