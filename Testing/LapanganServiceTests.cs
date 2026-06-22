996b4f70e9d3f429ca4e7f5eed360c617a5ac1cb
jkezka <nabilarosa79@gmail.com>
2026-06-22 19:45:18 +0700

    Menggabungkan testing lama dan testing auth

---
+++ Testing/LapanganServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tugas_Besar_CLO4.Services;
+using System.Linq;
+
+namespace Testing
+{
+    [TestClass]
+    public class LapanganServiceTests
+    {
+        [TestMethod]
+        public void GetLapanganByGedung_ShouldReturnOnlySelectedBuilding()
+        {
+            LapanganService service =
+                new LapanganService();
+
+            var hasil =
+                service.GetLapanganByGedung(
+                    "Gedung B"
+                );
+
+            Assert.AreEqual(
+                2,
+                hasil.Count
+            );
+
+            Assert.IsTrue(
+                hasil.All(
+                    x => x.lokasi == "Gedung B"
+                )
+            );
+        }
+    }
+}
