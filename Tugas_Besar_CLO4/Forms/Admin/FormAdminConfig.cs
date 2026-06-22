99eb6b701ff1f3dacd61173654f7eedcde92298e
Ravie <aryaravie8@gmail.com>
2026-06-22 17:47:31 +0700

    menggabungkan logic ke GUI

---
+++ Tugas_Besar_CLO4/Forms/Admin/FormAdminConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using Tugas_Besar_CLO4.Config;
+
+namespace Tugas_Besar_CLO4.Forms
+{
+    public partial class FormAdminConfig : Form
+    {
+        public FormAdminConfig()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAdminConfig_Load(object sender, EventArgs e)
+        {
+            decimal currentTax = ConfigService.Instance.GetTax() * 100;
+            decimal currentDiscount = ConfigService.Instance.GetDiscount() * 100;
+
+            txtTax.Text = currentTax.ToString("0.##");
+            txtDiscount.Text = currentDiscount.ToString("0.##");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal newTax = decimal.Parse(txtTax.Text) / 100m;
+                decimal newDiscount = decimal.Parse(txtDiscount.Text) / 100m;
+
+                ConfigService.Instance.UpdateConfig(newTax, newDiscount);
+
+                MessageBox.Show("Konfigurasi berhasil disimpan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Pastikan format input berupa angka yang valid.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Tugas_Besar_CLO4/Forms/Admin/FormRiwayatBooking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Tugas_Besar_CLO4.Services;
+using Tugas_Besar_CLO4.Entities;
+
+namespace Tugas_Besar_CLO4.Forms
+{
+    public partial class FormRiwayatBooking : Form
+    {
+        public FormRiwayatBooking()
+        {
+            InitializeComponent();
+            LoadRiwayatData();
+        }
+
+        private void LoadRiwayatData()
+        {
+            dgvBooking.Rows.Clear();
+            List<Booking> semuaRiwayat = BookingService.Instance.GetSemuaRiwayat();
+
+            foreach (Booking b in semuaRiwayat)
+            {
+                dgvBooking.Rows.Add(b.NamaPemesan, b.TipeLapangan, b.Durasi);
+            }
+        }
+
+        private void dgvBooking_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
